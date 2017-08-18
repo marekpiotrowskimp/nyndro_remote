11ba2b33ccb2fa556defb8f0baaa8d69ee16f751
Marek Piotrowski <marek.piotrowski.mp@gmail.com>
2017-08-18 23:17:06 +0200

    refactoring stats adapter

--- app/src/main/java/iso/piotrowski/marek/nyndro/statistics/StatsRecyclerViewAdaprer.java
+++ app/src/main/java/iso/piotrowski/marek/nyndro/statistics/StatsRecyclerViewAdaprer.java
@@ -1,5 +1,6 @@
 package iso.piotrowski.marek.nyndro.statistics;
 
+import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import iso.piotrowski.marek.nyndro.Model.AnalysisInfo;
 import iso.piotrowski.marek.nyndro.R;
 import iso.piotrowski.marek.nyndro.UIComponents.BarChart;
@@ -18,23 +21,31 @@ import iso.piotrowski.marek.nyndro.UIComponents.BarChart;
 /**
  * Created by Marek on 05.08.2016.
  */
-public class StatsRecyclerViewAdaprer extends RecyclerView.Adapter<StatsRecyclerViewAdaprer.StatsCardViewHolder>{
+public class StatsRecyclerViewAdaprer extends RecyclerView.Adapter<StatsRecyclerViewAdaprer.StatsCardViewHolder> {
 
-    public static int TYPE_EMPTY=1;
-    public static int TYPE_FULL=2;
     private HistoryAnalysis[] historyAnalysis;
-    private String [] days;
-    private String [] months;
-    private int colors[]={0xa52a2a, 0x7fff00, 0x6495ed, 0xdc143c, 0xff1493, 0x228b22, 0xffd700, 0x008000, 0xff4500, 0xFFA07A, 0x00FFFF, 0x90EE90};
+    private String[] days;
+    private String[] months;
+    private int colors[] = {0xa52a2a, 0x7fff00, 0x6495ed, 0xdc143c, 0xff1493, 0x228b22, 0xffd700, 0x008000, 0xff4500, 0xFFA07A, 0x00FFFF, 0x90EE90};
+    private enum TypeOfStats {
+        Full(0), Empty(1);
+        private int value;
+        TypeOfStats (int value){
+            this.value = value;
+        }
+        public int getValue(){
+            return this.value;
+        }
+    }
 
     @Override
     public StatsCardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        CardView cv;
-        if (viewType == TYPE_EMPTY)
-            cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_cardview_layout_empty,parent,false);
+        CardView cardView;
+        if (TypeOfStats.values()[viewType] == TypeOfStats.Empty)
+            cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_cardview_layout_empty, parent, false);
         else
-            cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_cardview_layout,parent,false);
-        return new StatsCardViewHolder(cv,viewType);
+            cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_cardview_layout, parent, false);
+        return new StatsCardViewHolder(cardView, viewType);
     }
 
     public void setHistoryAnalysis(HistoryAnalysis[] historyAnalysis) {
@@ -57,74 +68,73 @@ public class StatsRecyclerViewAdaprer extends RecyclerView.Adapter<StatsRecycler
         this.months = months;
     }
 
-    public class StatsCardViewHolder extends RecyclerView.ViewHolder{
-        CardView cv;
+    public class StatsCardViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.statystics_practice_image_id) ImageView practiceImageId;
+        @BindView(R.id.statystics_practice_name) TextView practiceName;
+        @BindView(R.id.statystics_practice_day) @Nullable TextView practiceDays;
+        @BindView(R.id.statystics_practice_average_week) @Nullable TextView practiceAverageWeeks;
+        @BindView(R.id.statystics_practice_average_month) @Nullable TextView practiceAverageMonths;
+        @BindView(R.id.statystics_practice_average_days) @Nullable TextView practiceAverageDays;
+        @BindView(R.id.statystics_practice_expected_end) @Nullable TextView practiceExpectedDay;
+        @BindView(R.id.statystics_bar_chart) @Nullable BarChart practiceBarChart;
+        @BindView(R.id.statystics_bar_chart_month) @Nullable BarChart practiceBarChartMonth;
+        CardView cardView;
         int viewType;
-        StatsCardViewHolder (CardView cv, int viewType){
-            super(cv);
-            this.cv=cv;
-            this.viewType=viewType;
+
+        StatsCardViewHolder(CardView cardView, int viewType) {
+            super(cardView);
+            this.cardView = cardView;
+            this.viewType = viewType;
+            ButterKnife.bind(this, this.cardView);
         }
     }
 
     @Override
     public int getItemViewType(int position) {
-        if (historyAnalysis[position].getAnalysisResult().size()<3)
-            return TYPE_EMPTY;
+        if (historyAnalysis[position].getAnalysisResult().size() < 3)
+            return TypeOfStats.Empty.getValue();
         else
-            return TYPE_FULL;
+            return TypeOfStats.Full.getValue();
     }
 
     @Override
     public void onBindViewHolder(StatsCardViewHolder holder, int position) {
 
-        if (holder.viewType==TYPE_FULL) {
-            ImageView practiceImageId = (ImageView) holder.cv.findViewById(R.id.statystics_practice_image_id);
-            TextView practiceName = (TextView) holder.cv.findViewById(R.id.statystics_practice_name);
-            TextView practiceDays = (TextView) holder.cv.findViewById(R.id.statystics_practice_day);
-            TextView practiceAverageWeeks = (TextView) holder.cv.findViewById(R.id.statystics_practice_average_week);
-            TextView practiceAverageMonths = (TextView) holder.cv.findViewById(R.id.statystics_practice_average_month);
-            TextView practiceAverageDays = (TextView) holder.cv.findViewById(R.id.statystics_practice_average_days);
-            TextView practiceExpectedDay = (TextView) holder.cv.findViewById(R.id.statystics_practice_expected_end);
-            BarChart practiceBarChart = (BarChart) holder.cv.findViewById(R.id.statystics_bar_chart);
-            BarChart practiceBarChartMonth = (BarChart) holder.cv.findViewById(R.id.statystics_bar_chart_month);
+        if (TypeOfStats.values()[holder.viewType] == TypeOfStats.Full) {
 
             Map<String, AnalysisInfo> historyAnalysisResult = historyAnalysis[position].getAnalysisResult();
 
             if (!historyAnalysisResult.isEmpty()) {
-                practiceImageId.setImageDrawable(holder.cv.getResources().getDrawable(historyAnalysisResult.get("practice_image_id").getNumber()));
-                practiceName.setText(" "+historyAnalysisResult.get("practice_name").toString());
-                practiceDays.setText(" "+historyAnalysisResult.get("practice_days").toString());
-                practiceAverageWeeks.setText(" "+historyAnalysisResult.get("average_week").toString());
-                practiceAverageMonths.setText(" "+historyAnalysisResult.get("average_month").toString());
-                practiceAverageDays.setText(" "+historyAnalysisResult.get("average_days").toString());
-                practiceExpectedDay.setText(" "+historyAnalysisResult.get("finish_practice").toString()+" dni\n"+
-                        historyAnalysisResult.get("finish_practice_date").toString());
+                holder.practiceImageId.setImageDrawable(holder.cardView.getResources().getDrawable(historyAnalysisResult.get("practice_image_id").getNumber()));
+                holder.practiceName.setText(String.format(" %s", historyAnalysisResult.get("practice_name").toString()));
+                if (holder.practiceDays != null) holder.practiceDays.setText(String.format(" %s", historyAnalysisResult.get("practice_days").toString()));
+                if (holder.practiceAverageWeeks != null) holder.practiceAverageWeeks.setText(String.format(" %s", historyAnalysisResult.get("average_week").toString()));
+                if (holder.practiceAverageMonths != null) holder.practiceAverageMonths.setText(String.format(" %s", historyAnalysisResult.get("average_month").toString()));
+                if (holder.practiceAverageDays != null) holder.practiceAverageDays.setText(String.format(" %s", historyAnalysisResult.get("average_days").toString()));
+                if (holder.practiceExpectedDay != null) holder.practiceExpectedDay.setText(String.format(" %s dni\n%s", historyAnalysisResult.get("finish_practice").toString(),
+                        historyAnalysisResult.get("finish_practice_date").toString()));
 
                 List<BarChart.DataObj> list = new ArrayList<BarChart.DataObj>();
                 for (int ind = 0; ind < 7; ind++) {
                     String key = "day_" + Integer.toString(ind);
                     int number = historyAnalysisResult.get(key).getNumber();
-                    list.add(new BarChart.DataObj(days[ind] + " > " + Integer.toString(number), number, ind<colors.length? colors[ind]:0xAAAAAA));
+                    list.add(new BarChart.DataObj(days[ind] + " > " + Integer.toString(number), number, ind < colors.length ? colors[ind] : 0xAAAAAA));
                 }
-                practiceBarChart.setDataChart(list);
+                if (holder.practiceBarChart != null) holder.practiceBarChart.setDataChart(list);
 
                 List<BarChart.DataObj> listMonth = new ArrayList<BarChart.DataObj>();
                 for (int ind = 0; ind < 12; ind++) {
                     String key = "month_" + Integer.toString(ind);
                     int number = historyAnalysisResult.get(key).getNumber();
-                    listMonth.add(new BarChart.DataObj(months[ind] + " > " + Integer.toString(number), number, ind<colors.length? colors[ind]:0xAAAAAA));
+                    listMonth.add(new BarChart.DataObj(months[ind] + " > " + Integer.toString(number), number, ind < colors.length ? colors[ind] : 0xAAAAAA));
                 }
-                practiceBarChartMonth.setDataChart(listMonth);
+                if (holder.practiceBarChartMonth != null) holder.practiceBarChartMonth.setDataChart(listMonth);
 
             }
-        } else
-        {
-            ImageView practiceImageId = (ImageView) holder.cv.findViewById(R.id.statystics_practice_image_id);
-            TextView practiceName = (TextView) holder.cv.findViewById(R.id.statystics_practice_name);
+        } else {
             Map<String, AnalysisInfo> historyAnalysisResult = historyAnalysis[position].getAnalysisResult();
-            practiceImageId.setImageDrawable(holder.cv.getResources().getDrawable(historyAnalysisResult.get("practice_image_id").getNumber()));
-            practiceName.setText(" "+historyAnalysisResult.get("practice_name").toString());
+            holder.practiceImageId.setImageDrawable(holder.cardView.getResources().getDrawable(historyAnalysisResult.get("practice_image_id").getNumber()));
+            holder.practiceName.setText(String.format(" %s", historyAnalysisResult.get("practice_name").toString()));
         }
     }
 
